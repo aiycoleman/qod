bffe53c007c3fca46554db62ac72f86f99017d7a
Aiyesha Coleman <2020152186@ub.edu.bz>
2025-08-26 20:08:57 -0600

    created userfriendly error messages

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -7,21 +7,19 @@ import (
 )
 
 // healthcheckHandler gives us the health of the system
-func (app *application) healthcheckHandler(w http.ResponseWriter,
+func (a *application) healthcheckHandler(w http.ResponseWriter,
 	r *http.Request) {
 
 	data := envelope{
 		"status": "available",
 		"system_info": map[string]string{
-			"environment": app.config.env,
-			"version":     app.config.version,
+			"environment": a.config.env,
+			"version":     a.config.version,
 		},
 	}
 
-	err := app.writeJSON(w, http.StatusOK, data, nil)
+	err := a.writeJSON(w, http.StatusOK, data, nil)
 	if err != nil {
-		app.logger.Error(err.Error())
-		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
-		return
+		a.serverErrorResponse(w, r, err)
 	}
 }
