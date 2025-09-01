5b8035ac366142e42501e0d907f8e71831aeab76
Aiyesha Coleman <2020152186@ub.edu.bz>
2025-08-31 19:04:33 -0600

    refactoring routes

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -10,7 +10,14 @@ import (
 
 // routes specifies our routes
 func (app *application) routes() http.Handler {
+	// setup a new routes
 	router := httprouter.New()
+
+	// handle 404
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+	// handle 405
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
+	// setup routes
 
 	// Define a GET route for health check
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
