bc432d3022453c92278314c3e43b979d0f74915d
Aiyesha Coleman <2020152186@ub.edu.bz>
2025-09-29 15:19:44 -0600

    user table fixed

--- migrations/000002_create_users_table.up.sql
+++ migrations/000002_create_users_table.up.sql
@@ -5,5 +5,6 @@ CREATE TABLE IF NOT EXISTS users(
     username text NOT NULL,
     email citext UNIQUE NOT NULL,
     password_hash bytea NOT NULL,
+    activated bool NOT NULL,
     version integer NOT NULL DEFAULT 1
 );
